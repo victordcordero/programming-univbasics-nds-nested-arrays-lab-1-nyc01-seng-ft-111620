d67143e2caefb6b5e9b8b275678200d8011d2ee2
Victor Cordero <victordcordero@gmail.com>
2020-11-04 20:58:23 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -38,7 +38,8 @@ end
 def matrix_update(matrix, row, column, new_value)
   row = CONVENTIONAL_PRODUCE
   column = ORGANIC_PRODUCE
-matrix[1][1]
+new_value = matrix
+
   # Given any matrix (array of arrays), a row index and a column index,
   # Update the matrix location at that row and column to have the value of new_value
   # Return the updated matrix
